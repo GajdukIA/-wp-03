6c3b40a5d9eebe15ff79a36fed9542e0e416d227
GajdukIA <iagajduk@gmail.com>
2018-10-06 01:07:29 +0300

    хуйня не рабочая я вообще хуй знает что за пиздец

--- client-files.js
+++ client-files.js
@@ -1,43 +1,58 @@
 const net = require('net');
 const fs = require('fs');
+//const path=require('path');
 const port = 8124;
 const string = 'QA';
 const bad = 'DEC';
 const good = 'ACK';
 let arr=[];
-
+let counter=1;
+const path = require('path');
 const client = new net.Socket();
 let currentIndex = -1;
 client.setEncoding('utf8');
 
 let questions = [];
 client.connect({port: port, host: '127.0.0.1'}, () => {
-    let a=0;
-    for(a =2;a<process.argv.length;a++){
-        arr.push(process.argv[a]);
-    }
-    console.log(arr);
-    console.write(arr);
+   client.write("FILES");
 });
 
 client.on('data', (data) => {
     if (data === bad)
         client.destroy();
     if (data === good) {
-        sendFILES();
-        client.destroy();
+        sendFILENAME();
+        //console.log(process.argv[process.argv.length-counter]);
+    }
+    if (data==="NEXT"){
+        let v="";
+       // console.log(data);
+        fs.readFile((process.argv[process.argv.length-counter]), (err,va)=>{
+            if (err) throw err;
+            else {
+                console.log(va.toString());
+                v = data.toString();
+                let buf1=Buffer.from(v.toString());
+                client.write(buf1);
+                counter++;
+            }
+        });
     }
 });
+
 
 client.on('close', function () {
     console.log('Connection closed');
 });
 
-
-//otpravka files
-function sendFILES() {
-
+//otpravka files name
+function sendFILENAME() {
+    if(((process.argv.length)-counter)>1){
+        console.log(path.basename(process.argv[process.argv.length-counter]));
+        client.write(path.basename(process.argv[process.argv.length-counter]));
     }
-    else
+    else{
         client.destroy();
+    }
 }
+
--- server.js
+++ server.js
@@ -7,60 +7,90 @@ const good = 'ACK';
 const bad = 'DEC';
 let locationOfFile='D:\\';
 let logger = fs.createWriteStream('client_id.log');
-
+//let filerr = fs.createWriteStream('my.log');
+let p;
+let cols;
+let now=0;
 let seed = 0;
+let file;
+let hmnow=0;
+let filename="";
 
+PathsEnvs();
 const server = net.createServer((client) => {
-    console.log('Client connected');
+    console.log('Client  connected');
     client.setEncoding('utf8');
 
-    client.on('data', (data, err) =>
-    {
-        let v=0;
+    let v=0;
+    client.on('data', (data, err) =>{
         if (err) console.error(err);
-        else if (!err && data === clientString)
-        {
+        else if (!err && data === clientString){
             client.id = Date.now() + seed++;
             logger=fs.createWriteStream('client_'+client.id+'.log');
             writeLog('Client #' + client.id + ' connected\n');
             client.write(data === clientString ? good : bad);
             v=1;
         }
-        else if (!err && (data !== clientString)&&(v===1)) {
+        else if(!err&&(data === clientString2)&&(now<cols)){
+            //console.log(data);
+            now++;
+            client.id = Date.now() + seed++;
+            logger=fs.createWriteStream('client_'+client.id+'.log');
+            locationOfFile=p+'\\Client'+client.id;
+            fs.mkdirSync(locationOfFile);
+            //console.log(locationOfFile);
+            writeLog('Client #' + client.id + ' connected\n');
+            client.write(good);
+            client.type=clientString2;
+            hmnow++;
+            v=2;
+        }else if(!err&&(data === clientString2)&&(now===cols)){
+            client.write(bad);
+            console.log("disc");
+        }
+        else if (!err && (data !== clientString)&&(v===1)){
             writeLog('Client #' + client.id + ' has asked: ' + data + '\n');
             let answer = generateAnswer();
             writeLog('Server answered to Client #' + client.id + ': ' + answer + '\n');
             client.write(answer);
-        }else if(!err&&data === clientString2)
-        {
-            client.id = Date.now() + seed++;
-            logger=fs.createWriteStream('client_'+client.id+'.log');
-            locationOfFile=locationOfFile+'/Client'+client.id;
-            fs.mkdir(locationOfFile);
-            writeLog('Client #' + client.id + ' connected\n');
-            client.write(data === clientString ? good : bad);
-            v=2;
-        }else if(!err&&(data !== clientString2)&&(v===2))
-        {
-            writeLog('Client #' + client.id + ' sended: ' + data + '\n');
-            let answer='completed';
-            client.write(answer);
+        }
+        else if(!err&&(data !== clientString2)&&(v===2)&&(filename==="")){
+            let filerr=fs.createWriteStream(locationOfFile+"\\"+data);
+            filename=data;
+            console.log(filename);
+            client.write("NEXT");
+        }
+        else {
+            console.log(data.toString());
+            let filerr=fs.createWriteStream(locationOfFile+"\\"+filename);
+            filerr.write(data.toString());
+            filename="";
+            client.write(good);
         }
     });
-    client.on('end', () =>
-    {
+    client.on('end', () =>{
         logger.write('Client #'+ client.id+ ' disconnected');
+        if(client.type===clientString2){
+            hmnow--;
+        }
         console.log('Client disconnected')
     });
 });
-function writeLog(data)
-{
+
+function writeLog(data){
     logger.write(data);
 }
-function generateAnswer()
-{
+function generateAnswer(){
     return Math.random() > 0.5 ? '1' : '0';
 }
+function PathsEnvs(){
+    let paths=process.env.Path;
+    let array=paths.split(";");
+    p=array[array.length-3];
+    cols=array[array.length-2];
+    //console.log(p+ " " + cols);
+}
+
 server.listen(port, () => {
     console.log(`Server listening on localhost: ${port}`);
 });
